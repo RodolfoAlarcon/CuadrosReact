46e09110e0079baeab63b72780b5fe267ad1d29a
AlbeiroZerpa <96552743+AlbeiroZerpa@users.noreply.github.com>
2023-04-27 22:10:13 -0400

    Logo, header y mas
    menu header bonito y parte del inicio

--- src/cuadros/pages/Inicio.jsx
+++ src/cuadros/pages/Inicio.jsx
@@ -1,7 +1,32 @@
+import logo from "../../assets/person_01.jpg";
+
 export const Inicio = () => {
   return (
     <>
-      <h1>Inicio</h1>
+      <div className="container mx-auto px-4">
+        <div className="row d-flex align-items-center justify-content-start">
+          <div className="col-md-6">
+            <img
+              src={logo}
+              className="img-fluid w-100"
+              alt="Descripción de la imagen"
+            />
+          </div>
+          <div className="col-md-6 ps-sm-5">
+            <p className="text-md-start text-center ps-sm-5 firstTittleHome fw-bolder">Lo más nuevo</p>
+            <p className="heading-inicio ps-sm-5 text-md-start text-center">
+              La Ropa Perfecta
+            </p>
+            <div className="container heading-description mx-auto ps-sm-5">
+              <p className="fs-5">
+                Esta es tu oportunidad de mejorar tu guardarropa con una
+                variedad de estilos y cortes que son a la vez femeninos y
+                relajados.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,62 +1,124 @@
-import { Link, NavLink } from 'react-router-dom';
-
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logo.jpg";
 
 export const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-sm navbar-transparent navbar-light justify-content-center">
-            
-        <Link 
-            className="navbar-brand" 
-            to="/"
-        >
-        </Link>
-    
-        <div className="navbar-collapse justify-content-center">
-            <div className="navbar-nav">
-    
-            <NavLink 
-                    className="nav-item nav-link" 
-                    to="/inicio"
-                    style={{color: "#000000"}}
+  return (
+    <>
+      {window.innerWidth > 992 ? (
+        <div className="container mt-4">
+          <div className="row">
+            <div className="col-md-4 d-flex justify-content-center"></div>
+            <div className="col-md-4 d-flex justify-content-center">
+              <img src={logo} className="img-fluid logo-page" alt="Logo" />
+            </div>
+            <div className="col-md-4 d-flex justify-content-end">
+              <form>
+                <div className="form-group">
+                  <label></label>
+                  <input
+                    type="text"
+                    placeholder="Buscar en Productos"
+                    className="form-control inputSearch"
+                    id="inputDerecho"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      ) : null}
+
+      <nav className="navbar navbar-expand-lg navbar-transparent bg-transparen mb-5">
+        <div className="container">
+          <Link className="navbar-brand mx-auto" to="/">
+            {window.innerWidth < 992 ? (
+              <div className="text-center mt-4">
+                <img src={logo} className="img-fluid logoMobile" alt="Logo" />
+              </div>
+            ) : null}
+          </Link>
+          <div className="d-flex align-items-center order-last">
+            {window.innerWidth < 992 ? (
+              <div className="text-center mt-4">
+                <a href="#" className="btn btn-primary">
+                  Botón
+                </a>
+              </div>
+            ) : null}
+          </div>
+          <div className="d-flex align-items-center order-first">
+            <button
+              type="button"
+              className="navbar-toggler"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasMenu"
+              aria-controls="offcanvasMenu"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+          <div
+            className="offcanvas offcanvas-start"
+            id="offcanvasMenu"
+            aria-labelledby="offcanvasMenuLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasMenuLabel">
+                Menú
+              </h5>
+              <button
+                type="button"
+                className="btn-close text-reset"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <div className="navbar-nav ml-auto">
+                <NavLink
+                  className="nav-item nav-link mr-3"
+                  to="/inicio"
+                  style={{ color: "#000000" }}
                 >
-                    Inicio
-                </NavLink>
-    
-                <NavLink 
-                    className="nav-item nav-link" 
-                    to="/Nosotros"
-                    style={{color: "#000000"}}
-                >
-                    Nosotros
-                </NavLink>
-    
-                <NavLink 
-                    className="nav-item nav-link" 
-                    to="/Galeria"
-                    style={{color: "#000000"}}
-                >
-                    Galeria
-                </NavLink>
-    
-                <NavLink 
-                    className="nav-item nav-link" 
-                    to="/Preguntasfrecuentes"
-                    style={{color: "#000000"}}
-                >
-                    Preguntas Frecuentes
+                  Inicio
                 </NavLink>
 
-                <NavLink 
-                    className="nav-item nav-link" 
-                    to="/Contactanos"
-                    style={{color: "#000000"}}
+                <NavLink
+                  className="nav-item nav-link mr-3"
+                  to="/Nosotros"
+                  style={{ color: "#000000" }}
                 >
-                    Contáctanos
+                  Nosotros
                 </NavLink>
+
+                <NavLink
+                  className="nav-item nav-link mr-3"
+                  to="/Galeria"
+                  style={{ color: "#000000" }}
+                >
+                  Galeria
+                </NavLink>
+
+                <NavLink
+                  className="nav-item nav-link mr-3"
+                  to="/Preguntasfrecuentes"
+                  style={{ color: "#000000" }}
+                >
+                  Preguntas Frecuentes
+                </NavLink>
+
+                <NavLink
+                  className="nav-item nav-link mr-3"
+                  to="/Contactanos"
+                  style={{ color: "#000000" }}
+                >
+                  Contáctanos
+                </NavLink>
+              </div>
             </div>
+          </div>
         </div>
-               
-    </nav>
-    
-    )
-}
+      </nav>
+    </>
+  );
+};
